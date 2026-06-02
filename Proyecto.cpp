ea1a2cfedbe3046b92cb8a674ba9e6508eb7c04b
Mega_Fish1506 <fa0743492@gmail.com>
2026-06-01 19:26:00 -0700

    Base del programa, primer menu inicial

---
+++ Proyecto.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int op;
+
+    using namespace std;
+    
+    do {
+        cout << "1.Nuevo Alumno" << endl;
+        cout << "2.Agregar servicio a un estudiante" << endl;
+        cout << "3.Modificar un servicio de un estudiante" << endl;
+        cout << "4.Reportes" << endl;
+        cout << "5.Elaboro (Nombre de todos los integrantes)" << endl;
+        cout << "6.Salir" << endl;
+        cout << "Ingrese una opcion: ";
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                break;
+            case 6:
+                break;
+            default:
+                break;
+        }
+    } while (op != 6);
+    
+    return 0;
+}
